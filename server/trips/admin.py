5e96459c27369d75c969f052f32a5ef0dc3237e5
Juan David H <juan.hernandez@anomali.com>
2021-08-11 17:08:07 -0500

    Updated admin.py

--- server/trips/admin.py
+++ server/trips/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(DefaultAdmin):
+    pass
