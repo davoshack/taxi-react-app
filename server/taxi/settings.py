09e6d11e75edf26a93435c12a512fce2c0cdc5a2
Juan David H <juan.hernandez@anomali.com>
2021-08-11 17:46:37 -0500

    Updated settings.py

--- server/taxi/settings.py
+++ server/taxi/settings.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 from pathlib import Path
@@ -123,4 +124,17 @@ CHANNEL_LAYERS = {
             'hosts': [REDIS_URL],
         },
     },
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
+    'USER_ID_CLAIM': 'id',
 }
